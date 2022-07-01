24e9ef496412c0d04cb22caccac14a580735a455
CarlosMC2003 <carlitosenrike@gmail.com>
2022-07-01 17:15:20 -0500

    Taller11-Cambios2.0

--- Taller11/src/paquete03/MenuCarta.java
+++ Taller11/src/paquete03/MenuCarta.java
@@ -3,17 +3,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete03;
-
+import paquete02.*;
 /**
  *
  * @author Carlos Morocho - PC
  */
-public class MenuCarta {
-   private double valorPorcionGuar;
+public class MenuCarta extends Menu {
+   private double valorPorciGuar;
    private double valorBebida;
    private double porcentaje;
    
-   public MenuCarta() {
-       
+   public MenuCarta(String nombre, double valorIni,
+           double porci, double bebi, double porce) {
+       super(nombre, valorIni);
+       valorPorciGuar = porci;
+       valorBebida = bebi;
+       porcentaje = porce;
+   }
+   
+   public void establecerValorPorciones(double n) {
+       valorPorciGuar = n;
+   }
+   
+   public void establecerValorBebida(double n) {
+       valorBebida = n;
+   }
+   
+   public void establecerPorcentajeAdicional(double n) {
+       porcentaje = n / 100;
+   }
+   
+   @Override
+   public void calcularValorMenu(){
+       valorMen = valorPorciGuar + valorBebida + (porcentaje * valorIniMen);
+   }
+   
+   public double obtenerValorPorciones() {
+       return valorPorciGuar;
+   }
+   
+   public double obtenerValorBebida() {
+       return valorBebida;
+   }
+   
+   public double obtenerPorcentajeAdicional() {
+       return porcentaje;
+   }
+   
+   public String toString() {
+       String cadena = String.format("Menu a la Carta:\n"
+               + "", );
    }
 }
